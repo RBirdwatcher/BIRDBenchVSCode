27fc96defa682fea1569593c1e7bd34afe92f837
RBirdwatcher <neil@birdsoftwaresolutions.com>
2022-12-29 01:44:23 +0000

    Changed Branding

--- org.eclipse.efbt.openregspecs.dsl.ide/src/org/eclipse/efbt/openregspecs/dsl/ide/ServerLauncher.java
+++ org.eclipse.efbt.openregspecs.dsl.ide/src/org/eclipse/efbt/openregspecs/dsl/ide/ServerLauncher.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016, 2022 itemis AG (http://www.itemis.de) and others.
+ * Copyright (c) 2016, 2022 BirdSoftwareSolutions Ltd (http://www.BirdSoftwareSolutions.com) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
